ef1eef2843e4b20bee80bf3f9899e3f75a98cac0
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-05-11 17:50:32 +0530

    feat(firebase) : SetUp firebase

---
+++ firebase/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { app } from './firebase/firebase'
+const App = () => {
+  return (
+    <div> FireBase App</div>
+  )
+}
+
+export default App
---
+++ firebase/src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGESENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
+};
+
+export const app = initializeApp(firebaseConfig);
