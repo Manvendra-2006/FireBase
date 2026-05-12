48763fa3b2743b6a968f5d1c79b0535c05317cf8
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-05-12 11:40:14 +0530

    feat(firestore) : to get data by query

--- CloudFireStore/databse/src/App.jsx
+++ CloudFireStore/databse/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { getFirestore , collection,addDoc , doc,getDoc} from 'firebase/firestore'
+import { getFirestore , collection,addDoc , doc,getDoc, where , query, getDocs} from 'firebase/firestore'
 import { app } from './Firebase/Firebase'
 const firestore = getFirestore(app) // instance create 
 const App = () => {
   const writeData = async () =>{
     console.log("Button Clicked")
     try{
- const result = await addDoc(collection(firestore,"cities"),{
+ const result = await addDoc(collection(firestore,"cities"),{ //This will tell how to write a document inside a collection
       name:"Delhi",
       pinCode:1234,
       lat:123,
@@ -20,31 +20,40 @@ const App = () => {
   console.log(err)
    }
   }
-  const writeSubCollection = async () =>{
-   
+  const writeSubCollection = async () =>{  // This tell how to write a collection inside a document
     try{
  const result = await addDoc(collection(firestore,"cities/cHjbA3cJBYGKO36ug2pi/places"),{
       name:"This is a place",
       des:"Awesome descrrip",
       good:true
     })
+    console.log(result)
     }
     catch(err){
       console.log(err)
     }
   }
-  const getDocument = async () =>{
+  const getDocument = async () =>{  // This will get the document or a data of specific path
     const res =  doc(firestore,"cities","UQNNuqDhSOqyc6joUdWU")
     const data = await getDoc(res)
     console.log(data.data())
   }
   getDocument()
+
+  const getDocumentByQuery = async ()=>{ // This will get the particular document or collection 
+      const collectionRef = collection(firestore,"userdata12345/document67890/purchase13579")
+      const q = query(collectionRef,where("Car","==",987689100))
+      const sanpshot = await getDocs(q)
+      sanpshot.forEach((data)=>console.log(data.data()))
+  }
+
   return (
     <div> 
       <h1>firebase app</h1>
       <button  type="button" onClick={writeData} >Put Data</button>
       <button type="button" onClick={writeSubCollection}>Put SubCollection</button>
       <button type='button' onClick={getDocument}>Get Document</button>
+      <button type='button' onClick={getDocumentByQuery}>GetDocumentbyquery</button>
     </div>
   )
 }
